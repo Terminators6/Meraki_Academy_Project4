a1d42b7f1a467da7edf9874a1107e3f7df4fa8cf
M_abedalnabi <abedalnabi96@gmail.com>
2021-06-03 17:23:24 +0300

    Createnewuser

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,25 +1,16 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
-    firstName = { type: String },
-    lastName = { type: String },
-    age = { type: Number },
-    country = { type: String },
-    email = { type: String },
-    password = { type: String },
-    image = { type: String },
-    role = { type: mongoose.Schema.Types.ObjectId , ref:"Role"} 
-})
+  firstName: { type: String },
+  lastName: { type: String },
+  age: { type: Number },
+  country: { type: String },
+  email: { type: String },
+  password: { type: String },
+  image: { type: String },
+  role: { type: mongoose.Schema.Types.ObjectId, ref: "Role" },
+});
 
-module.exports = mongoose.model('User',userSchema)
-
-
-
-
-
-
-
-
-
+module.exports = mongoose.model("User", userSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const db = require("./db/db");
-
 const app = express();
 
 //routers
+const { usersRouter } = require("./routers/routes/users");
 
 //built-in middlewares
 app.use(express.json());
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 //app routers
+app.use("/user", usersRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {

7d0febb3f17208530d0759de4123d987e67bb1ca
maisaa <maisaa.alkhedr@gmail.com>
2021-06-03 20:44:18 +0200

    edit files

--- backend/routers/routes/users.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { createNewUser } = require("./../controllers/users");
-
-const usersRouter = express.Router();
-
-usersRouter.post("/", createNewUser);
-
-module.exports = usersRouter;

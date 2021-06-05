991d80d20bcb0c33d21d02739637f67d484e95a5
M_abedalnabi <abedalnabi96@gmail.com>
2021-06-05 23:12:05 +0300

    favButton

--- backend/routers/controllers/favorite.js
+++ backend/routers/controllers/favorite.js
@@ -14,7 +14,24 @@ const favorite = async (req, res) => {
     return res.status(404).json(err);
   }
 };
+/*
+const favButton = async (req, res) => {
+  const serviceId = req.params;
+  const userId = req.body.id;
 
+  const a = await User.findOne({ _id: userId }, "favorite");
+  let b = a.favorite;
+  let c = b.filter((ele, i) => {
+    return ele === serviceId;
+  });
+  if (c.length > 0) {
+    const user = await User.findOneAndUpdate({ _id: userId }, { favorite: { $push: { serviceId } } });
+    res.json(user);
+  } else {
+    return;
+  }
+};
+*/
 module.exports = {
   favorite,
 };

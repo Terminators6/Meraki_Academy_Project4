79ebb1187ab0980839f8658d6a66cdd51d396fc7
BayanDaboubash <bayan.daboubash2000@gmail.com>
2021-06-06 18:44:02 +0300

    authorization

--- backend/routers/middlewares/authentication.js
+++ backend/routers/middlewares/authentication.js
@@ -1,24 +1,24 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const secret = process.env.SECRET;
+// const jwt = require('jsonwebtoken');
+// const bcrypt = require('bcrypt');
+// const secret = process.env.SECRET;
 
-const authentication = (req , res , next)=>{
-    if(req.headers.authorization){
-        const token = req.headers.authorization.splite(" ").pop();
-        jwt.verify(token,secret,(err, result)=>{
-            if(result){
-                console.log(token);
-                req.token = result;
-                console.log("result......",result )
-                next();
-            }
-            else{
-                res.status(403).json("The token is invalid or expired");
-            }
-        })
-    }
-    else{
-        res.json("No headers .authorization are found");
+// const authentication = (req , res , next)=>{
+//     if(req.headers.authorization){
+//         const token = req.headers.authorization.splite(" ").pop();
+//         jwt.verify(token,secret,(err, result)=>{
+//             if(result){
+//                 console.log(token);
+//                 req.token = result;
+//                 console.log("result......",result )
+//                 next();
+//             }
+//             else{
+//                 res.status(403).json("The token is invalid or expired");
+//             }
+//         })
+//     }
+//     else{
+//         res.json("No headers .authorization are found");
         
-    }
-}
+//     }
+// }
--- backend/routers/middlewares/authorization.js
+++ backend/routers/middlewares/authorization.js
@@ -1,1 +1,18 @@
-const jwt = require('jsonwebtoken');
+// const jwt = require("jsonwebtoken");
+// const Role = require("./../../db/models/role");
+
+// const authorization = (permission) => {
+//   return function (req, res, next) {
+//     Role.findById({ _id: req.token.role }).then((result) => {
+//       const perm = result.permission;
+//       if (perm.includes(permission)) {
+//         next();
+//       } else {
+//         result.status(403).json("forbidden");
+//       }
+//     })
+//     .catch((err)=>{
+//         res.send("There is no role");
+//     })
+//   };
+// };

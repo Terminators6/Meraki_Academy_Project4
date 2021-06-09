30a158242c040274f06e86ee1f0aba3671e0a680
maisaa <maisaa.alkhedr@gmail.com>
2021-06-09 22:03:28 +0200

    done with edit profile

--- frontend/src/components/profile/index.js
+++ frontend/src/components/profile/index.js
@@ -8,46 +8,50 @@ import './profile.css'
 
 export const Profile = () => {
     const profileContext = useContext(ProfileContext);
-    
+
     const [edit, setEdit] = useState(true);
 
 
     useEffect(() => {
-        console.log('....Profile useEffect', profileContext.user);
         profileContext.getUserProfile();
+        console.log('....Profile useEffect', profileContext.user);
+        
     }, [])
 
-
+    const handelSubmit = (e) => {
+        e.preventDefault();
+        profileContext.updateUserProfile();
+    }
 
     return (
 
         <div>
             {edit ?
-                <div className="Profile" >
+                <form className="Profile" onSubmit={handelSubmit}>
                     <div>
                         <label>First Name</label>
-                        <input type="text" placeholder="First Name here" value={profileContext.user.firstName} />
+                        <input type="text" defaultValue={profileContext.user.firstName} onChange={(e) => profileContext.setFirstName(e.target.value)} />
                     </div>
                     <div>
                         <label>Last Name</label>
-                        <input type="text" placeholder="Last Name here" value={profileContext.user.lastName} />
+                        <input type="text" defaultValue={profileContext.user.lastName} onChange={(e) => profileContext.setLastName(e.target.value)} />
                     </div>
                     <div>
                         <label>Age</label>
-                        <input type="Number" placeholder="Age here" value={profileContext.user.age} />
+                        <input type="Number" defaultValue={profileContext.user.age} onChange={(e) => profileContext.setAge(e.target.value)} />
                     </div>
                     <div>
                         <label>Country</label>
-                        <input type="text" placeholder="Country here" value={profileContext.user.country} />
+                        <input type="text" defaultValue={profileContext.user.country} onChange={(e) => profileContext.setCountry(e.target.value)} />
                     </div>
                     <div>
                         <label>Email</label>
-                        <input type="text" placeholder="Email here" value={profileContext.user.email} />
+                        <input type="text" value={profileContext.user.email}  onChange={(e)=> profileContext.setEmail(e.target.value)}/>
                     </div>
                     {/* <input type="password" placeholder="Password here" onChange={(e) => registerContext.setPassword(e.target.value)} /> */}
-                    <div><button onClick={profileContext.updateUserProfile()}>Save Changes</button> <button>Delete Account</button></div>
+                    <div><button>Save Changes</button> </div>
                     {/* {registerContext.message && <div> {registerContext.message} </div>} */}
-                </div> : <div></div>}
+                </form> : <div></div>}
         </div>
 
     )

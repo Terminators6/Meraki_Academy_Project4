e9b67e433833e09a8d7fd081ac5ab25971d3dc0b
IbrahimBaniSalim <ibrahim.banisalim@gmail.com>
2021-06-09 22:09:36 +0300

    edit

--- frontend/src/components/seconedPage/allSupService.js
+++ frontend/src/components/seconedPage/allSupService.js
@@ -1,18 +1,30 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect,useState } from 'react';
 import {AllSupServiceContext} from '../../contexts/secondaPage/AllSupService';
 
 const AllSupService = () => {
+	
 	const allSupServiceContext = useContext(AllSupServiceContext);
-    
+    const [item, setItem] = useState([]);
+    useEffect(() => {
+		allSupServiceContext.getAllSupService();
+
+		
+	}, [])
 	
 
 	const handleClick = () => {
-		allSupServiceContext.getAllSupService();
-		
-        allSupServiceContext.setShow(!allSupServiceContext.show);
+		const a=allSupServiceContext.service
+		setItem(a.map((elem)=>{
+			return elem.image
+		}))
+		console.log(a);
+		console.log(item,"itemitem");
+        // allSupServiceContext.setShow(!allSupServiceContext.show);
+		console.log(allSupServiceContext.service,"gggggggggggg");
+
 	};
-
-
+	
+    
 
 	
 
@@ -22,7 +34,7 @@ const AllSupService = () => {
 			<br />
 			<h1>allServices</h1>
 			<button onClick={handleClick}>Get All supService</button>
-			{/* <img src="" onclik={} /> */}
+			 <img src={item} onClick={} />
 		</>
 	);
 };

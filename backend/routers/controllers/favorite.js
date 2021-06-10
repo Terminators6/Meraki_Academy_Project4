8b34d831e2461c98e8b562970aaf8b9f0d064817
M_abedalnabi <abedalnabi96@gmail.com>
2021-06-10 12:46:20 +0300

    rate

--- backend/routers/controllers/favorite.js
+++ backend/routers/controllers/favorite.js
@@ -1,33 +1,37 @@
 const User = require("./../../db/models/user");
 const supServiceModule = require("./../../db/models/supService");
 
-const favorite = async (req, res) => {
-  const id = req.params;
-  try {
-    const FAV = await User.findOne({ _id: id }, "favorite");
-    return res.status(200).json(FAV);
-  } catch (err) {
-    return res.status(404).json(err);
-  }
-};
-/*
-const favButton = async (req, res) => {
-  const serviceId = req.params;
-  const userId = req.body.id;
+// const favorite = async (req, res) => {
+//   const id = req.params;
+//   try {
+//     const FAV = await User.findOne({ _id: id }, "favorite");
+//     return res.status(200).json(FAV);
+//   } catch (err) {
+//     return res.status(404).json(err);
+//   }
+// };
 
-  const a = await User.findOne({ _id: userId }, "favorite");
-  let b = a.favorite;
-  let c = b.filter((ele, i) => {
-    return ele === serviceId;
-  });
-  if (c.length > 0) {
-    const user = await User.findOneAndUpdate({ _id: userId }, { favorite: { $push: { serviceId } } });
-    res.json(user);
-  } else {
-    return;
-  }
-};
-*/
+// const favButton = async (req, res) => {
+// const { userId, serviceId } = req.body;
+
+// console.log("userid", userid);
+// const a = await User.findOne({ _id: userId }, "favorite");
+// let b = a.favorite;
+// let c = b.filter((ele, i) => {
+//   return ele === serviceId;
+// });
+// if (c.length > 0) {
+//   const user = await User.findOneAndUpdate(
+//     { _id: userId },
+//     { favorite: { $push: { serviceId } } }
+//   );
+//   res.json(user);
+// } else {
+//   return;
+// }
+// };
+
 module.exports = {
-  favorite,
+  // favorite,
+  // favButton,
 };

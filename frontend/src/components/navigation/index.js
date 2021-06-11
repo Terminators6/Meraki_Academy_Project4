3c50e1e8d0d9f9da2ea338539af8189a11f9312c
M_abedalnabi <abedalnabi96@gmail.com>
2021-06-11 20:32:45 +0300

    style

--- frontend/src/components/navigation/index.js
+++ frontend/src/components/navigation/index.js
@@ -11,17 +11,26 @@ export const Navigation = () => {
   const toggleDarkMode = () => setDarkMode(darkMode ? false : true);
 
   return (
-    <div>
-
+    <div className="Navbar">
       {localStorage.getItem("token") && loginContext.loggedIn ? (
+        <div className="Nav">
+          <Link to="/">
+            {" "}
+            <img className="NavIm" src="https://rb.gy/xkgfkm" />{" "}
+          </Link>
 
-        <div className="Nav">  
-          <Link to="/"> <img  className="NavIm" src="https://rb.gy/xkgfkm" /> </Link>
-
-          <Link to="/Favorite" className="styleText"> Favorite </Link>
-          <Link to="/Profile" className="styleText"> Profile </Link>
-          <Link to="/logout" className="styleText styleLogOut"> Log Out </Link>
-
+          <Link to="/Favorite" className="styleText">
+            {" "}
+            Favorite{" "}
+          </Link>
+          <Link to="/Profile" className="styleText">
+            {" "}
+            Profile{" "}
+          </Link>
+          <Link to="/logout" className="styleText styleLogOut">
+            {" "}
+            Log Out{" "}
+          </Link>
         </div>
       ) : (
         <div className="Nav">
@@ -30,8 +39,14 @@ export const Navigation = () => {
             <img className="NavIm" src="https://rb.gy/xkgfkm" />{" "}
           </Link>
 
-          <Link to="/Register" className="styleText"> Register </Link>
-          <Link to="/Login" className="styleText"> Login </Link>
+          <Link to="/Register" className="styleText">
+            {" "}
+            Register{" "}
+          </Link>
+          <Link to="/Login" className="styleText">
+            {" "}
+            Login{" "}
+          </Link>
         </div>
       )}
     </div>

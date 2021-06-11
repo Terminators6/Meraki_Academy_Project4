09f9fa0035ca7b52d7367c0a803830953c43b904
maisaa <maisaa.alkhedr@gmail.com>
2021-06-11 14:06:33 +0200

    fix save changes

--- frontend/src/contexts/profile.js
+++ frontend/src/contexts/profile.js
@@ -6,7 +6,6 @@ import axios from 'axios';
 
 import jwt from "jsonwebtoken";
 import { LoginContext } from "./login";
-import { useHistory } from "react-router-dom";
 
 let userId;
 
@@ -14,7 +13,7 @@ export const ProfileContext = React.createContext();
 
 const ProfileProvider = (props) => {
 
-  const history = useHistory();
+    const history = useHistory();
 
     const [user, setUser] = useState({ "Name": "T" });
     const [firstName, setFirstName] = useState('');
@@ -25,71 +24,61 @@ const ProfileProvider = (props) => {
     const [password, setPassword] = useState('');
     const [role, setRole] = useState('60be297bc1ce0378e4564f2a');
     const [message, setMessage] = useState('...');
- 
 
 
-  const [user, setUser] = useState({ Name: "T" });
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [age, setAge] = useState("");
-  const [country, setCountry] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [role, setRole] = useState("60be297bc1ce0378e4564f2a");
-
-  const loginContext = useContext(LoginContext);
+    const loginContext = useContext(LoginContext);
 
 
-  const state = {
-    setUser,
-    user,
-    setFirstName,
-    firstName,
-    lastName,
-    age,
-    country,
-    email,
-    setLastName,
-    setAge,
-    setCountry,
-    setEmail,
-    setPassword,
-    setRole,
-    getUserProfile,
-    updateUserProfile,
-    deleteUserProfile,
-  };
+    const state = {
+        setUser,
+        user,
+        setFirstName,
+        firstName,
+        lastName,
+        age,
+        country,
+        email,
+        setLastName,
+        setAge,
+        setCountry,
+        setEmail,
+        setPassword,
+        setRole,
+        getUserProfile,
+        updateUserProfile,
+        deleteUserProfile,
+    };
 
 
-  const token = loginContext.token || localStorage.getItem("token");
+    const token = loginContext.token || localStorage.getItem("token");
 
-  function validationToken() {
-    const user = jwt.decode(token);
-    // if (!userId) {
-    //     console.log(".........token error !userId ", user)
-    //     // throw new Error();
-    // }
-    // else {
-    userId = user.userId;
-    // }
-  }
-
-  async function getUserProfile() {
-    console.log("get User Profile.......");
-    try {
-      validationToken();
-      const res = await axios.get(`http://localhost:5000/profile/${userId}`);
-      setUser(res.data);
-      setFirstName(res.data.firstName);
-      setLastName(res.data.lastName);
-      setAge(res.data.age);
-      setCountry(res.data.country);
-      setEmail(res.data.email);
-      console.log("....setUser res.data", res.data);
-    } catch (error) {
-      console.log("error in getProfile frontend", error);
+    function validationToken() {
+        const user = jwt.decode(token);
+        // if (!userId) {
+        //     console.log(".........token error !userId ", user)
+        //     // throw new Error();
+        // }
+        // else {
+        userId = user.userId;
+        // }
     }
-  }
+
+    async function getUserProfile() {
+        console.log("get User Profile.......");
+        try {
+            validationToken();
+            const res = await axios.get(`http://localhost:5000/profile/${userId}`);
+            setUser(res.data);
+            setFirstName(res.data.firstName);
+            setLastName(res.data.lastName);
+            setAge(res.data.age);
+            setCountry(res.data.country);
+            setEmail(res.data.email);
+            console.log("....setUser res.data", res.data);
+        } catch (error) {
+            console.log("error in getProfile frontend", error);
+        }
+    }
 
 
     async function updateUserProfile() {
@@ -101,42 +90,42 @@ const ProfileProvider = (props) => {
             await axios.put(`http://localhost:5000/profile/${userId}`, user)
                 .then((response) => {
                     setMessage("User Profile Updated Successfully");
-                    // history.push('/Profile');
+                    history.push('/Profile');
                     console.log('....Edit User profile ..', response)
                 })
 
         } catch (error) {
-                setMessage("Error happened while update, Please try again");
-                history.push('/Profile');
-                console.log("error in editUserProfile frontend", error)
+            setMessage("Error happened while update, Please try again");
+            history.push('/Profile');
+            console.log("error in editUserProfile frontend", error)
         }
 
     }
-  }
 
-  async function deleteUserProfile() {
+
+async function deleteUserProfile() {
     await localStorage.clear();
 
 
     console.log("delete User Profile.......");
     try {
-      validationToken();
-      const res = await axios.delete(`http://localhost:5000/profile/${userId}`);
-      setUser(res.data);
-      console.log("....Delete User profile res.data", res.data);
-      setTimeout(() => {
-        history.push("/");
-         setMessage("User Profile Deleted Successfully");
-        localStorage.clear();
-      }, 2000);
+        validationToken();
+        const res = await axios.delete(`http://localhost:5000/profile/${userId}`);
+        setUser(res.data);
+        console.log("....Delete User profile res.data", res.data);
+        setTimeout(() => {
+            history.push("/");
+            setMessage("User Profile Deleted Successfully");
+            localStorage.clear();
+        }, 2000);
     } catch (error) {
         setMessage("Error happened while delete user profile, Please try again");
-      console.log("error in deleteUserProfile frontend", error);
+        console.log("error in deleteUserProfile frontend", error);
 
     }
-  }
+}
 
-  return <ProfileContext.Provider value={state}>{props.children}</ProfileContext.Provider>;
+return <ProfileContext.Provider value={state}>{props.children}</ProfileContext.Provider>;
 };
 
 export default ProfileProvider;
